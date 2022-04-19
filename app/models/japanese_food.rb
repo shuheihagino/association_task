6c2d9af6efd2fbc9ff040ed75e190584f9bd8b2c
shuhei_hagino <shuhei_hagino@mediado.jp>
2022-04-19 17:00:47 +0900

    6th

--- app/models/japanese_food.rb
+++ app/models/japanese_food.rb
@@ -1,6 +1,6 @@
 class JapaneseFood < ActiveRecord::Base
-    belongs_to :shop
-    has_many :order_foods
-    has_many :orders, through: :order_foods
+    #belongs_to :shop
+    #has_many :order_foods
+    #has_many :orders, through: :order_foods
 
 end
--- app/models/order_food.rb
+++ app/models/order_food.rb
@@ -2,8 +2,8 @@ class OrderFood < ApplicationRecord
     belongs_to :food
     belongs_to :order
     
-    belong_to :chinese_foods
-    belong_to :italian_foods
-    belong_to :japanese_foods
+    #belong_to :chinese_foods
+    #belong_to :italian_foods
+    #belong_to :japanese_foods
 
 end
